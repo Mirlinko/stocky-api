b77121b61394002ba11d5a2d0f56c45e112d6c37
Nazar <mikliush.nazar@gmail.com>
2025-05-25 21:16:49 +0200

    save last query to save free api calls

--- src/constants/chart.constants.ts
+++ src/constants/chart.constants.ts
@@ -1,1 +1,5 @@
 export const ALPHA_VANTAGE_API_KEY = "CQEGGIO2A1SCAO2P";
+
+export const TIME_SERIES: Record<string, string> = {
+  DAILY: "DAILY"
+}
--- src/modules/chart/chart.service.ts
+++ src/modules/chart/chart.service.ts
@@ -1,24 +1,32 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
-import { ALPHA_VANTAGE_API_KEY } from 'src/constants/chart.constants';
+import { ALPHA_VANTAGE_API_KEY, TIME_SERIES } from 'src/constants/chart.constants';
 import { formatMetaData, transformAlphaVantageToCandlestick } from 'src/helpers/data';
 
 @Injectable()
 export class ChartService {
+  lastQueryData: any;
+
   constructor(
     private HttpService: HttpService
   ) {}
 
   async getDataByTicker(ticker: string): Promise<any> {
     try {
-      const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${ALPHA_VANTAGE_API_KEY}`;
+      if (this.lastQueryData) {
+        return { data: this.lastQueryData, error: undefined };
+      }
+
+      const url = this.getTimeSeriesUrl(TIME_SERIES.DAILY, ticker);
 
       const { data } = await firstValueFrom(
         this.HttpService.get(url)
       );
 
       const dataToReturn = this.buildData(data);
+
+      this.lastQueryData = dataToReturn;
 
       return { data: dataToReturn, error: undefined };
     } catch(error) {
@@ -41,5 +49,12 @@ export class ChartService {
     }
 
     return dataToReturn;
+  }
+
+  private getTimeSeriesUrl(series: string, ticker: string) {
+    switch(series) {
+      case TIME_SERIES.DAILY: 
+        return `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&outputsize=full&apikey=${ALPHA_VANTAGE_API_KEY}`;
+    }
   }
 }
